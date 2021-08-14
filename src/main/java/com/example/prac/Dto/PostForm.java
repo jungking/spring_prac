ff1c899796a2337298d658e771d6e3f3907059bd
jungking <immenige2@gmail.com>
2021-08-14 11:31:21 +0900

    update : fix post err, append post list

--- src/main/java/com/example/prac/Dto/PostForm.java
+++ src/main/java/com/example/prac/Dto/PostForm.java
@@ -1,6 +1,5 @@
 package com.example.prac.Dto;
 
-import com.example.prac.Domain.PostEntity;
 import lombok.*;
 
 @Getter
@@ -13,18 +12,8 @@ public class PostForm {
     private String content;
     private String userid;
 
-    public PostEntity toEntity(){
-        PostEntity postEntity = PostEntity.builder()
-                .id(id)
-                .title(title)
-                .content(content)
-                .userid(userid)
-                .build();
-        return postEntity;
-    }
-
     @Builder
-    public void postForm(Long id, String title, String content, String userid) {
+    public PostForm(Long id, String title, String content, String userid) {
         this.id = id;
         this.title = title;
         this.content = content;
--- src/main/java/com/example/prac/Repository/PostRepository.java
+++ src/main/java/com/example/prac/Repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.example.prac.Repository;
 
 import com.example.prac.Domain.PostEntity;
+import com.example.prac.Dto.PostForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
