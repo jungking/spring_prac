eb65c8606e8d0c0450ab8b57199ae87f0d8cd9e6
jungking <immenige2@gmail.com>
2021-09-03 15:22:23 +0900

    append : http session userid

--- src/main/java/com/example/prac/Controller/UserController.java
+++ src/main/java/com/example/prac/Controller/UserController.java
@@ -3,17 +3,14 @@ package com.example.prac.Controller;
 import com.example.prac.Dto.AccountForm;
 import com.example.prac.Service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
